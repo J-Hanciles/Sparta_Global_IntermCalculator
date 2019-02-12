135e71e0a454401c9ecc05529e3a5974aac1adfe
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-02-12 16:36:41 +0000

    setup of if statement without issue

---
+++ IntermCalculator/IntermCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntermCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type 'm' for math calculator, otherwise type 'a' for area calculator");
+            char choice1 = (char)Console.Read();
+            if (choice1 == (char)'m')
+            {
+
+                int num1 = 0;
+                int num2 = 0;
+               
+            }
+            else if (choice1 == (char)'a')
+            {
+                Console.WriteLine('a');
+            }
+            else
+            {
+                Console.WriteLine("Help me, help you, pick an option");
+            }
+        }
+    }
+}
