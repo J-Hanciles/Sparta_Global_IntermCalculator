a453be3e6122ccb6b5df9b0164a79dd5bb8f54b7
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-02-12 23:18:25 +0000

    math and area calculator complete, may look at loops in the morning

--- IntermCalculator/IntermCalculator/Program.cs
+++ IntermCalculator/IntermCalculator/Program.cs
@@ -11,21 +11,100 @@ namespace IntermCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Type 'm' for math calculator, otherwise type 'a' for area calculator");
-            char choice1 = (char)Console.Read();
-            if (choice1 == (char)'m')
+            string choice1 = Console.ReadLine();
+            if (choice1 == "m")
+            {
+            
+                double num1 = 0;
+                double num2 = 0;
+                string operand;
+                double ans = 0;
+
+                Console.WriteLine("Please enter the first integer: ");
+                num1 = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter an operand (+, -, /, *): ");
+                operand = Console.ReadLine();
+                Console.WriteLine("Please enter the second integer: ");
+                num2 = Double.Parse(Console.ReadLine());
+                
+
+                switch (operand)
+                {
+                    case "-":
+                        ans = num1 - num2;
+                        break;
+                    case "+":
+                        ans = num1 + num2;
+                        break;
+                    case "/":
+                        ans = num1 / num2;
+                        break;
+                    case "*":
+                        ans = num1 * num2;
+                        break;
+                }
+                Console.WriteLine(num1 + " " + operand + " " + num2 + " " + "Your Answer is " + ans);
+                Console.ReadLine();
+            }
+            else if (choice1 == "a")
             {
 
-                int num1 = 0;
-                int num2 = 0;
-               
-            }
-            else if (choice1 == (char)'a')
-            {
-                Console.WriteLine('a');
+                double height = 0;
+                double width = 0;
+                double radius = 0;
+                double circans = 0;
+                double trians = 0;
+                double recans = 0;
+
+
+                Console.WriteLine("Circle, Triangle or Rectangle?");
+                string choice2 = Console.ReadLine();
+
+                if (choice2 == "Circle")
+                {
+                    Console.WriteLine("Enter Circle radius please");
+                    radius = Double.Parse(Console.ReadLine());
+
+                    circans = 3.142 * (radius * radius);
+                    Console.WriteLine("The area of your Circle is " + circans);
+                    Console.ReadLine();
+
+                }
+                else if (choice2 == "Triangle")
+                {
+                    Console.WriteLine("Enter Triangle Height");
+                    height = Double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Triangle Width");
+                    width = Double.Parse(Console.ReadLine());
+
+                    trians = 0.5 * height * width;
+                    Console.WriteLine("The area of your Triangle is " + trians);
+                    Console.ReadLine();
+
+                }
+                else if (choice2 == "Rectangle")
+                {
+                    Console.WriteLine("Enter Rectangle Height");
+                    height = Double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Rectangle Height");
+                    width = Double.Parse(Console.ReadLine());
+
+                    recans = height * width;
+                    Console.WriteLine("The area of your Rectangle is " + recans);
+                    Console.ReadLine();
+
+                }
+                else
+                {
+                    Console.WriteLine("You think this is a game");
+                    Console.ReadLine();
+                }
+
             }
             else
             {
                 Console.WriteLine("Help me, help you, pick an option");
+                Console.ReadLine();
             }
         }
     }
